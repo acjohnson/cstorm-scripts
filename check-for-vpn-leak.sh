2b715012694596463bba001fa55531805e7ff460
Aaron Johnson <acjohnson@pcdomain.com>
2019-06-29 11:42:58 -0500

    switched to transmission...

--- check-for-vpn-leak.sh
+++ check-for-vpn-leak.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 #
 
-startQbittorrent () {
+startTransmission () {
   QB_PID=$1
 
   if [ -z "${QB_PID}" ]; then
-    echo "Qbittorrent not running, starting..."
-    systemctl start qbittorrent
+    echo "Transmission not running, starting..."
+    systemctl start transmission-daemon
   else
-    echo "Qbittorrent is already running..."
+    echo "Transmission is already running..."
   fi
 }
 
-stopQbittorrent () {
+stopTransmission () {
   QB_PID=$1
 
   if [ -z "${QB_PID}" ]; then
-    echo "Qbittorrent is already stopped..."
+    echo "Transmission is already stopped..."
   else
-    echo "Qbittorrent is running, stopping..."
-    systemctl stop qbittorrent
+    echo "Transmission is running, stopping..."
+    systemctl stop transmission-daemon
   fi
 }
 
@@ -29,15 +29,15 @@ startVPN () {
   if [ "${VPN_STATE}" == "active" ]; then
     echo "VPN is already running..."
     echo "Restarting VPN service to attempt to resolve DNS leak..."
-    systemctl restart openvpn@cstorm.service
+    systemctl restart openvpn-client@cstorm.service
   else
     echo "VPN not running, starting..."
-    systemctl start openvpn@cstorm.service
+    systemctl start openvpn-client@cstorm.service
   fi
 }
 
 installNewToken () {
-  TOKEN=$(/opt/cstorm-scripts/imap-client.sh)
+  TOKEN=$(/root/cstorm-scripts/imap-client.sh)
   echo "${TOKEN}" > /etc/openvpn/cstorm-creds-conf
   echo "thisisnotapassword" >> /etc/openvpn/cstorm-creds-conf
 }
@@ -47,8 +47,8 @@ START_DATE_TIME=$(eval "${DATE_TIME}")
 echo "==== START VPN CHECK ===="
 echo "${START_DATE_TIME}"
 
-QB_PID_CMD="ps -ef | grep qbittorrent | grep -v grep | awk '{print $2}'"
-VPN_STATE_CMD="systemctl is-active openvpn@cstorm.service"
+QB_PID_CMD="ps -ef | grep transmission-daemon | grep -v grep | awk '{print $2}'"
+VPN_STATE_CMD="systemctl is-active openvpn-client@cstorm.service"
 NS_CMD="grep -v ^# /etc/resolv.conf | grep ^nameserver | head -1 | awk '{print $2}'"
 
 # Check to see if VPN is working
@@ -59,7 +59,7 @@ if [[ $? -ne 0 ]]; then
   NS=$(eval "${NS_CMD}")
   echo "Non-local nameserver detected: ${NS}, VPN appears to be working!"
   QB_PID=$(eval "${QB_PID_CMD}")
-  startQbittorrent "${QB_PID}"
+  startTransmission "${QB_PID}"
   echo "Exiting..."
 else
   NS=$(eval "${NS_CMD}")
@@ -67,7 +67,7 @@ else
   echo "Attempting to download and install the latest token!"
   installNewToken
   QB_PID=$(eval "${QB_PID_CMD}")
-  stopQbittorrent "${QB_PID}"
+  stopTransmission "${QB_PID}"
   echo "Attempting to start VPN..."
   VPN_STATE=$(eval "${VPN_STATE_CMD}")
   startVPN "${VPN_STATE}"
@@ -78,7 +78,7 @@ else
     NS=$(eval "${NS_CMD}")
     echo "Non-local nameserver detected: ${NS}, VPN leak fixed!"
     QB_PID=$(eval "${QB_PID_CMD}")
-    startQbittorrent "${QB_PID}"
+    startTransmission "${QB_PID}"
     echo "Exiting..."
   else
     NS=$(eval "${NS_CMD}")
